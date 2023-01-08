4af67c7c7eb59e3b47f99ac472150c6fffa0f32f
Miguel Areiza <miangelareiza@gmail.com>
2023-01-07 19:39:27 -0500

    api deploy

--- scripts/login.js
+++ scripts/login.js
@@ -28,29 +28,57 @@ $(document).ready(()=>{
         const USER = e.target[0].value;
         const PASS = e.target[1].value;
 
-        fetch('../../data.txt').then(async (response) => {
+        // fetch('../../data.txt').then(async (response) => {
 
-            let users = (await response.json()).users;
+        //     let users = (await response.json()).users;
             
-            users = users.filter(user => user.user == USER && user.pass == PASS);
+        //     users = users.filter(user => user.user == USER && user.pass == PASS);
 
-            if (users.length === 1) {
+        //     if (users.length === 1) {
                 
-                let AppUser = [{id:users[0].id,
-                                user:users[0].user,
-                                name:users[0].name,
-                                activo:users[0].acti}];
-                sessionStorage.setItem('AppUser', JSON.stringify(AppUser));
-                // location.href = './portal.html'
-                history.pushState(null, "", "../portal/");        
-                goLocation.ChangeView('./');
+        //         let AppUser = [{id:users[0].id,
+        //                         user:users[0].user,
+        //                         name:users[0].name,
+        //                         activo:users[0].acti}];
+        //         sessionStorage.setItem('AppUser', JSON.stringify(AppUser));
+        //         // location.href = './portal.html'
+        //         history.pushState(null, "", "../portal/");        
+        //         goLocation.ChangeView('./');
 
-            } else {
-                toastr.Warning('Usuario o contraseña invalida');
-            }
+        //     } else {
+        //         toastr.Warning('Usuario o contraseña invalida');
+        //     }
             
-        }).catch(() => {
+        // }).catch(() => {
+        //     toastr.Error('Error en la transaccion');
+        // });
+
+        var requestOptions = {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                "procedure": `sp_ValidateUser '${USER}', '${PASS}';`
+            }),
+            redirect: 'follow'
+        };
+          
+        fetch('http://www.appescolar.somee.com/api/Procedures/ExecProcedure', requestOptions).then(async (response) => {
+        
+            let data = (await response.json())[0];
+            debugger;
+            if (!data.rpta) {
+                sessionStorage.setItem('AppUser', JSON.stringify(data));
+                history.pushState(null, "", "../portal/");        
+                goLocation.ChangeView('./');                
+            } else {
+                toastr.Warning('Usuario o contraseña invalidos');
+            }
+
+        }).catch((e) => {
             toastr.Error('Error en la transaccion');
+            console.log('Error', e);
         });
 
     });
